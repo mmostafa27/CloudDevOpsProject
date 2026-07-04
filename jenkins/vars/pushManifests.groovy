86eefe29dc9bb928cf3e561d589a7a2ed2c2f920
mmostafa27 <mm3724658@gmail.com>
2026-07-05 01:02:50 +0300

    Update shared library

--- jenkins/vars/pushManifests.groovy
+++ jenkins/vars/pushManifests.groovy
@@ -1,9 +1,17 @@
 def call(String repoUrl) {
-    sh """
-        git config user.email "jenkins@ivolve.com"
-        git config user.name "Jenkins"
-        git add kubernetes/deployment.yml
-        git commit -m "Update image tag to build ${BUILD_NUMBER}"
-        git push ${repoUrl} main
-    """
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'github-credentials',
+            usernameVariable: 'GIT_USERNAME',
+            passwordVariable: 'GIT_PASSWORD'
+        )
+    ]) {
+        sh """
+            git config user.email "jenkins@ivolve.com"
+            git config user.name "Jenkins"
+            git add kubernetes/deployment.yml
+            git commit -m "Update image tag to build ${BUILD_NUMBER}"
+            git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/mmostafa27/CloudDevOpsProject.git main
+        """
+    }
 }
